53d44771138ebe0ee8c1c7549b07efaa8071a948
Isuru Nishadha <isurunishadha99@gmail.com>
2023-03-28 09:35:25 +0530

    Added login and signup buttons

--- src/app/pages/home/components/product-box/product-box.component.ts
+++ src/app/pages/home/components/product-box/product-box.component.ts
@@ -1,18 +1,30 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 import { Product } from 'src/app/models/product.model';
 
 @Component({
   selector: '[app-product-box]',
   templateUrl: './product-box.component.html',
+  styleUrls: ['./product-box.component.css'],
 })
 export class ProductBoxComponent {
   @Input() fullWidthMode = false;
   @Input() product: Product | undefined;
   @Output() addToCart = new EventEmitter();
 
-  constructor() {}
+  @Input() myData: string | undefined;
+
+  constructor(private activatedRoute: ActivatedRoute) {}
+
+  ngOnItit(): void {}
+
+  bgColor = 'gray';
 
   onAddToCart(): void {
     this.addToCart.emit(this.product);
+  }
+
+  changeColor(): void {
+    this.bgColor = 'yellowgreen';
   }
 }
